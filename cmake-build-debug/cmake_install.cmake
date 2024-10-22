151374dd38aad579ef55eb86e324bd791d503594
Maaaikol <mreachu@myuax.com>
2024-10-22 11:10:09 +0200

    1ER EJ

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/micha/Documents/GitHub/ej-caso-final-integrador
+# Install script for directory: C:/Users/micha/Documents/GitHub/Caso_final_integrador/ej-caso-final-integrador
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/micha/Documents/GitHub/ej-caso-final-integrador/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/micha/Documents/GitHub/Caso_final_integrador/ej-caso-final-integrador/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
